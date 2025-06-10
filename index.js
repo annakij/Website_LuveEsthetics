847342063b5112be19e9f9bbcd7d13845c14212b
Anna Kijlstra <annakijlstra99@gmail.com>
2025-06-11 00:32:55 +0200

    loooots of changes, added individual treatment pages, all photos added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const products = [
       time: "45 minuter",
       price: "1 699",
       image: "images/enecta-cannabis-extracts-80wCkpt-IKE-unsplash.jpg",
+      details: "treatmentPages/vitamindropp.html",
       description:
         "Ge din kropp den näring den behöver med skräddarsydda vitamindroppar! I samarbete med Vitaminkliniken erbjuder vi en säker och effektiv behandling som ger din kropp viktiga vitaminer och mineraler direkt via dropp.",
     },
@@ -11,7 +12,8 @@ const products = [
       name: "Botox, 1-3 områden",
       time: "30 minuter",
       price: "1 900 - 2 900",
-      image: "images/botox.jpg",
+      image: "cardImages/botoxHeader.jpeg",
+      details: "treatmentPages/botox.html",
       description:
         "Botoxbehandling är idealisk för att släta ut rynkor och förebygga djupa veck. Botox fungerar som muskelavslappnande läkemedel för att förbättra linjer, särskilt i pannan, arga rynkar och kråksparkar, samt kan lindra migrän.",
     },
@@ -19,17 +21,19 @@ const products = [
       name: "Fillers, olika behandlingar",
       time: "30-45 minuter",
       price: "1 900 - 2 400",
-      image: "images/icons8-team-y2T5hT7pWx4-unsplash.jpg",
+      image: "cardImages/fillersHeader.jpeg",
+      details: "treatmentPages/fillers.html",
       description:
         "Fillers används för att fylla ut och lyfta huden, vilket ger ett slätare utseende och reducerar linjer och veck. Under behandlingen injiceras fillern i det område som du och din behandlare har kommit överens om. Eftersom effekten av behandlingen inte är permanent, krävs regelbundna återbesök för att upprätthålla det önskade resultatet.",
     },
     {
-      name: "Lashlift / Browlift",
+      name: "Lashlift",
       time: "60-70 minuter",
       price: "700 - 1 200",
-      image: "images/sunny-ng-KVIlNRoGwxk-unsplash.jpg",
+      image: "cardImages/lashliftHeader.jpeg",
+      details: "treatmentPages/lashlift.html",
       description:
-        "Brow och lash lift är skönhetsbehandlingar som ger dina ögonbryn och fransar en naturlig lyft och böjning. Behandlingarna förbättrar både form och volym, vilket ger ett fräschare och mer öppet uttryck utan behov av smink. Resultatet varar i flera veckor och ger ett hållbart, vackert resultat.",
+        "Lash lift är skönhetsbehandlingar som ger dina fransar en naturlig lyft och böjning. Behandlingarna förbättrar både form och volym, vilket ger ett fräschare och mer öppet uttryck utan behov av smink. Resultatet varar i flera veckor och ger ett hållbart, vackert resultat.",
     },
   ];
 
@@ -39,30 +43,30 @@ const products = [
 
     products.forEach((product) => {
       const productCard = `
-    <div class="card m-5 reveal">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img loading="lazy" src="${product.image}" class="img-fluid" alt="${product.name}">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body d-flex flex-column justify-content-between h-100">
-            <h3 class="card-title pt-4">${product.name}</h3>
-            <p>${product.time}</p>
-            <p class="card-text">${product.description}</p>
-            <p>${product.price} kr</p>
-            <a class="btn ms-auto"
-               class="footer-link"
-               style="background-color: #897d6e; color: #fff;"
-               aria-current="page"
-               href="https://www.bokadirekt.se/places/luve-esthetics-59235"
-               target="_blank">
-               Boka behandling
-            </a>
+      <div class="card my-5 reveal custom-card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img loading="lazy" src="${product.image}" class="img-fluid" alt="${product.name}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body d-flex flex-column justify-content-between h-100">
+              <h3 class="card-title pt-4">${product.name}</h3>
+              <p>${product.time}</p>
+              <p class="card-text">${product.description}</p>
+              <div class="d-flex justify-content-end">
+              <a class="btn custom-btn me-3" href="${product.details}">
+                VISA MER
+              </a>
+              <a class="btn custom-btn" href="https://www.bokadirekt.se/places/luve-esthetics-59235" target="_blank">
+                BOKA BEHANDLING
+              </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  `;
+    `;
+    
       container.innerHTML += productCard;
     });
 
